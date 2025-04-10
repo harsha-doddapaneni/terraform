7271eba5860b57a3854e147c1ec7b25ab7408dae
harsha-doddapaneni <harshadoddapaneni1@gmail.com>
2025-04-10 08:13:30 +0530

    Initial Terraform expense configuration

---
+++ expense/variables.tf
@@ -0,0 +1,21 @@
+variable "instance_names" {
+    type = list(string)
+    default = ["mysql", "backend", "frontend"]
+}
+
+variable "common_tags" {
+    type = map
+    default = {
+        Project = "expense"
+        Environment = "dev"
+        Terraform = "true"
+    }
+}
+
+variable "zone_id" {
+    default = "Z0177980G4CMKCIQPSDN"
+}
+
+variable "domain_name" {
+    default = "doddapaneni.shop"
+}
