4fdd6cac247d127b6488161d565ab130f94f5ecb
harsha-doddapaneni <harshadoddapaneni1@gmail.com>
2025-06-08 08:51:15 +0530

    terraform

--- conditions/provider.tf
+++ conditions/provider.tf
@@ -1,13 +1,12 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = "5.94.1"
+      source  = "hashicorp/aws"
+      version = "~> 5.94.0" # Allows minor updates within the 5.94.x range
     }
   }
 }
 
 provider "aws" {
-  # Configuration options
   region = "us-east-1"
 }
